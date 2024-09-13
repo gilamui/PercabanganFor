12f0036af05325ec8796840a0df9d2f27b5d0f44
gilamui <mattew_marante_33rpl@student.smktelkom-mlg.sch.id>
2024-09-13 10:45:18 +0700

    Add files via upload

---
+++ nilaiSiswa.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class nilaiSiswa {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Masukkan total siswa: ");
+        int totalSiswa = s.nextInt();
+
+        int nilai=0;
+        int totalNilai = 0;
+
+        for (int i = 1; i < totalSiswa; i++) {
+            System.out.print("Masukkan nilai untuk siswa " +i+ ": ");
+            nilai = s.nextInt();
+            totalNilai += nilai;
+        }
+
+        double ratarata = totalNilai / totalSiswa;
+
+        System.out.println("Rata-rata: " + ratarata);
+        s.close();
+    }
+}
